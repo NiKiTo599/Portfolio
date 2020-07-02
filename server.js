74ae06cf6e6fdddba2b02218ea16882515ba882c
NiKiTo599 <akulichnikita0@gmail.com>
2020-07-02 12:11:45 +0300

    fix: change route to index.html

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
 });
